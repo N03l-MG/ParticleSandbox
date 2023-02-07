4026b2cdf86d8da8abd3873bca90842c5944cc08
NO3L-MG <noelmonzongarcia@gmail.com>
2023-02-07 23:46:53 +0100

    Initial Commit

---
+++ Assets/Scripts/clickAndDrag.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickAndDrag : MonoBehaviour
+{
+    // Script that manages particle interactions and controls
+    
+    private Vector3 dragOffset;
+    private Camera cam;
+    private SpriteRenderer sprite;
+    public int sortingOrder = 0;
+    [SerializeField] private float speed = 10f;
+    //[SerializeField] public GameObject selectedParticle, reactionParticle, resultParticle;
+    //public string selectedParticleName, reactionParticleName, resultParticleName;
+
+    // camera is called when the scene is innitiated
+    void Awake()
+    {
+        cam = Camera.main;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    // calculate drag offset and ordering layer, then transform the particle to the mouse pos
+    void OnMouseDown()
+    {
+        dragOffset = transform.position - GetMousePos();
+        sprite.sortingOrder += 1;
+        //selectedParticleName = selectedParticle.name;
+    }
+    void OnMouseDrag()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, GetMousePos() + dragOffset, speed * Time.deltaTime);
+    }
+    // determine the mouse pos
+    Vector3 GetMousePos()
+    {
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+    // determine if the particles react, and intanciating the result accordingly
+    /*private void OnMouseUp()
+    {
+        Collider2D collider = Physics2D.OverlapPoint(transform.position);
+        if (collider != null)
+        {
+            if (gameObject.name == "Proton" && collider.gameObject.name == "Electron")
+            {
+                Instantiate(resultParticle, collider.transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(collider.gameObject);
+            }
+            else if (gameObject.name == "Electron" && collider.gameObject.name == "Proton")
+            {
+                Instantiate(resultParticle, collider.transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(collider.gameObject);
+            }
+            else 
+            {
+                collider.isTrigger = true;
+            }
+        }
+    }*/
+}
---
+++ Assets/Scripts/merge.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class merge : MonoBehaviour
+{
+    private static Dictionary<string, string> _recipes = new Dictionary<string, string>()
+    {
+        { "Proton + Electron", "Hydrogen" },
+        {"Hydrogen(Clone) + Proton", "Helium" }
+        //clean up later lol
+    };
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        var otherGameObject = collision.gameObject;
+        Debug.Log($"{gameObject} collided with {otherGameObject}");
+
+        var recipe = $"{gameObject.name} + {otherGameObject.name}";
+        if (!_recipes.ContainsKey(recipe)) return;
+        Instantiate(Resources.Load(_recipes[recipe]), (otherGameObject.transform.position + gameObject.transform.position) / 2, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(otherGameObject);
+    }
+}
+
+
+
+
+
+/* public class merge : MonoBehaviour
+{
+    public class ParticlePair
+    {
+        public string particle1;
+        public string particle2;
+        public GameObject resultParticlePrefab;
+
+        public ParticlePair(string particle1, string particle2, GameObject resultParticlePrefab)
+        {
+            this.particle1 = particle1;
+            this.particle2 = particle2;
+            this.resultParticlePrefab = resultParticlePrefab;
+        }
+    }
+
+    public List<ParticlePair> particlePairs = new List<ParticlePair>();
+    void Start(GameObject resultParticlePrefab)
+    {
+        
+        particlePairs.Add(new ParticlePair("Proton", "Electron", resultParticlePrefab));
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        foreach (ParticlePair particlePair in particlePairs)
+        {
+            if ((gameObject.name == particlePair.particle1 && collider.gameObject.name == particlePair.particle2) || (gameObject.name == particlePair.particle2 && collider.gameObject.name == particlePair.particle1))
+            {
+                collider.isTrigger = true;
+                Instantiate(particlePair.resultParticlePrefab, (transform.position + collider.transform.position) / 2, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(collider.gameObject);
+                break;
+            }
+            else {
+                collider.isTrigger = false;
+            }
+        }
+    }
+}
+*/
